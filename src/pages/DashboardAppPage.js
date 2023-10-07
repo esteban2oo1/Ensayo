2f37d5b883aa7e655e752f7cd90a168faf5afada
mclguerrero <maycolguerrero2021@itp.edu.co>
2023-10-06 22:19:01 -0500

    pedido de josa

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -31,7 +31,7 @@ export default function DashboardAppPage() {
 
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome back
+          chirretes inzano
         </Typography>
 
         <Grid container spacing={3}>
